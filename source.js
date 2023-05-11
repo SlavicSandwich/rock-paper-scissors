abb708a2cce3c981824c9640d3b20e0645f17533
SlavicSandwich <78306203+SlavicSandwich@users.noreply.github.com>
2023-05-12 00:59:41 +0300

    Finalize the main function and create game loop

--- source.js
+++ source.js
@@ -11,10 +11,40 @@ function getPlayerChoice() {
         return 1;
     case 'scissors':
         return 2;
+    default:
+        return null;
   }
 }
 
+function playMatch(playerChoice, computerChoice){
+    if (playerChoice == computerChoice){return 2}
+    if (playerChoice > computerChoice && !(playerChoice==2 && computerChoice == 0) || (playerChoice == 0 && computerChoice == 2)) {
+        return 1;
+    }
+    else{
+        return 0;
+    }
 
+}
 
-console.log(getComputerChoice());
-console.log(getPlayerChoice())
+function align(choice){
+    switch(choice){
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scirssors"
+    }
+}
+while (true){
+    let computerChoice = getComputerChoice()
+    let playerChoice = getPlayerChoice()
+    if (playerChoice === null){continue;}
+    console.log(`${align(playerChoice)} vs ${align(computerChoice)}...\n`)
+    switch(playMatch(playerChoice, computerChoice)){
+        case 1:{console.log("Player wins!"); break;}
+        case 0:{console.log("Computer wins!"); break;}
+        case 2:{console.log("Tie!"); break;}
+    }
+}
