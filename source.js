d58681a4e92ffa3b4ed6345bf6bfba7e5ffca240
SlavicSandwich <78306203+SlavicSandwich@users.noreply.github.com>
2023-05-28 01:31:38 +0300

    rework the execute, to_string, to_num functions

--- source.js
+++ source.js
@@ -2,19 +2,19 @@ function getComputerChoice() {
   return Math.floor(Math.random() * 3);
 }
 
-function getPlayerChoice() {
-  const choice = prompt("What's your choice?").toLowerCase();
-  switch (choice){
-    case 'rock':
-        return 0;
-    case 'paper':
-        return 1;
-    case 'scissors':
-        return 2;
-    default:
-        return null;
-  }
-}
+// function getPlayerChoice() {
+//   const choice = prompt("What's your choice?").toLowerCase();
+//   switch (choice){
+//     case 'rock':
+//         return 0;
+//     case 'paper':
+//         return 1;
+//     case 'scissors':
+//         return 2;
+//     default:
+//         return null;
+//   }
+// }
 
 function playMatch(playerChoice, computerChoice){
     if (playerChoice == computerChoice){return 2}
@@ -30,21 +30,21 @@ function playMatch(playerChoice, computerChoice){
 function to_string(choice){
     switch(choice){
         case 0:
-            return "rock";
+            return "✊";
         case 1:
-            return "paper";
+            return "✋";
         case 2:
-            return "scirssors"
+            return "✌"
     }
 }
 
 function to_num(choice){
     switch(choice){
-        case 'Rock':
+        case '✊':
             return 0;
-        case 'Paper':
+        case '✋':
             return 1;
-        case "Scissors":
+        case "✌":
             return 2;
     }
 }
@@ -60,11 +60,6 @@ function set_winner(winner){
 function restart(endgame){
     const scoreboard = document.querySelector('.scoreboard');
     const scorebox = document.querySelector('.scorebox');
-    if (endgame) {
-        h3 = document.querySelector('h3');
-        h3.textContent = '';
-    }
-    
     scorebox.querySelector('#winner').textContent = '';
     scores = scoreboard.querySelectorAll('span');
     scores.forEach((e)=> e.textContent = '0');
@@ -75,32 +70,28 @@ function stopShake(e){
 }
 function execute(){
     const scorebox = document.querySelector('.scorebox');
-    let res = (playMatch(to_num(this.textContent), getComputerChoice()))
+
+
+    const playerChoice = to_num(this.textContent);
+    const computerChoice = getComputerChoice();
+    let res = (playMatch(playerChoice, computerChoice));
+
+    scorebox.querySelector("#player").querySelector('.sign').textContent = to_string(playerChoice);
+    scorebox.querySelector("#computer").querySelector('.sign').textContent = to_string(computerChoice);
+
 
     switch(res){
-           case 1:{scorebox.querySelector('#winner').textContent = "Player wins!"; break;}
-           case 0:{scorebox.querySelector('#winner').textContent ="Computer wins!"; break;}
-           case 2:{scorebox.querySelector('#winner').textContent ="Tie!"; break;}
+           case 1:{scorebox.querySelector('.score-info').textContent = "Player wins!"; break;}
+           case 2:{scorebox.querySelector('.score-info').textContent ="Tie!"; break;}
+           case 0:{scorebox.querySelector('.score-info').textContent ="Computer wins!"; break;}
     }
 
-    
-    const scoreboard = document.querySelector('.scoreboard');
-    console.log(scorebox.querySelector('h3'))
-    if (scorebox.querySelector('h3').textContent != ''){
-        restart(1)
-    }
-    if (set_winner(res) == 'tie') return
-    score = scoreboard.querySelector(`#${set_winner(res)}`).querySelector('span')
-    score.parentNode.parentNode.classList.add('shake');
-    score.textContent = +score.textContent + 1;
-    if (score.textContent == 5){
-        h3 = document.querySelector('h3');
-        h3.textContent = `${set_winner(res)} wins!`
+    if (set_winner(res) != 'tie'){
+        score = scorebox.querySelector(`#${set_winner(res)}`).querySelector('span');
+        score.textContent = +score.textContent + 1;
     }
 }
 const restart_button = document.querySelector('.restart')
 restart_button.addEventListener('click', ()=>{restart(0)});
 const buttons = document.querySelectorAll('.choice')
 buttons.forEach((button) => button.addEventListener('click', execute))
-const scoretexts= document.querySelectorAll('.score');
-scoretexts.forEach(score=>score.addEventListener('animationend', stopShake))
